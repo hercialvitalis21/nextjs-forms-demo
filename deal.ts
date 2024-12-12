288a95d4e4f7df8212f41505daf0d7f5551582f5
Hercial <164944190+hercialvitalis21@users.noreply.github.com>
2024-12-12 04:03:31 -0400

    Add files via upload

--- deal.ts
+++ deal.ts
@@ -1,15 +1,15 @@
-import { z } from "zod";
+export interface DealFormState<T> {
+    errors: StringMap;  
+    successMsg?: string;
+    data?: T;
+    blurs?: StringToBooleanMap;
+}
 
-export const dealSchema = z.object({
-    name: z.string({message: "Name is required"}).min(6, {
-        message: "Name must be at least 6 characters long"
-    }),
-    link: z.string({message:"link is required"}).url({
-        message: "Please enter a valid URL starting with http:// or https://"
-    }),
-    price: z.coerce.number().min(2, {
-        message: "Price must be at least 2 dollars"
-    }),
-    couponCode: z.string({message:"Coupon code is required"}).min(5,{message:"Coupon code must be at least 5 characters long"}),
-    discount: z.coerce.number({message: "discount percentage is required"}).min(1, {message:"Discount must be at least 0%"}).max(100, {message:"Discount percentage  must be greater than 1%"}).max(100,{message:"Discount percentage  must be less than or equal to 100%"}),
-})
+export interface StringMap {
+    [key: string]: string;
+}
+
+export interface StringToBooleanMap {
+    [key: string]: boolean;
+}
+
